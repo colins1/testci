679d3e7651fa8fe2e3343097f1e678011dfd52b3
colins1 <maxrozhkov1@gmail.com>
2022-06-19 13:42:53 +0300

    ad

--- src/js/Character.js
+++ src/js/Character.js
@@ -2,34 +2,25 @@ import params from '../js/params';
 
 export default class Character {
   constructor (name, type) {
-    this.name = this.nameEdit(name);
-    this.type = this.typeEdit(type);
+    if (name.length < 2) {
+      throw new Error('min 3 string');
+    }else if (name.length > 10){
+      throw new Error('max 10 string');
+    }else if (typeof name !== 'string') {
+      throw new Error('please edit to string');
+    }else {
+      this.name = name;
+    }
+    if (type in params) {
+      this.type = type;
+    } else {
+      throw new Error('you selected a non-existent type');
+    }
     this.health = 100;
     this.level = 1;
     this.attack = params[this.type].attack;
     this.defence = params[this.type].defense;
   }
-
-  
-  nameEdit(name) {
-    if (name.length < 2) {
-     throw new Error('min 3 string');
-    }
-    if (name.length > 10) {
-      throw new Error('max 10 string');
-    }
-    if (typeof name !== 'string') {
-      throw new Error('please edit to string');
-    }
-      return name;
-  };
-
-  typeEdit(type) {
-    if (type in params) {
-      return type;
-    }
-    throw new Error('you selected a non-existent type');
-  };
 }
 
 
